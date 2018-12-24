c289051520f614d8ca380f60296eafff94eb379e
Alexander Voronkin <alexander.voronkin@gmail.com>
2018-12-24 17:49:26 +0300

    contact form

--- footer.php
+++ footer.php
@@ -22,8 +22,19 @@
     <p><a href="policy.php">Политика конфиденциальности</a></p>
     <p>(c) 2018 cirkovaphoto</p>
   </div>
-  <div class="col-sm-3">
-
+  <div class="col-sm-3 bordered-div">
+    <div class="row">    
+        <div class="form-group">          
+            <input id="form_name" type="text" name="name" class="form-control" placeholder="Please enter your firstname *" required="required" data-error="Firstname is required.">
+            <div class="help-block with-errors"></div>
+            <input id="form_email" type="email" name="email" class="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required.">
+            <div class="help-block with-errors"></div>
+            <input id="form_phone" type="tel" name="phone" class="form-control" placeholder="Please enter your phone">
+            <div class="help-block with-errors"></div>
+            <textarea id="form_message" name="message" class="form-control" placeholder="Message for me *" rows="4" required="required" data-error="Please,leave us a message."></textarea>
+            <div class="help-block with-errors"></div>
+        </div>    
+    </div>    
   </div>
   <div class="col-sm-3">
     <ul class="nav flex-column">        
