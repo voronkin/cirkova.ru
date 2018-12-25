0656839be78c098f43632707e8fc833537e4c992
Alexander Voronkin <alexander.voronkin@gmail.com>
2018-12-25 14:40:37 +0300

    contact form added

--- busgal.php
+++ busgal.php
@@ -71,16 +71,9 @@
         <iframe width="560" height="315" src="https://www.youtube.com/embed/nXPDRSK1mYg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>        
       </div>
   
+      
+      </div>      
       <!-- footer-->   
       <?php include 'footer.php';?> 
-      </div>      
-
-    
-    
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   </body>
 </html>
--- footer.php
+++ footer.php
@@ -23,18 +23,27 @@
     <p>(c) 2018 cirkovaphoto</p>
   </div>
   <div class="col-sm-3 bordered-div">
+    <form id="contact-form" method="post" action="contact.php" role="form">
+
+    <div class="messages"></div>
+
+    <div class="controls">
     <div class="row">    
         <div class="form-group">          
-            <input id="form_name" type="text" name="name" class="form-control" placeholder="Please enter your firstname *" required="required" data-error="Firstname is required.">
+            <input id="form_name" type="text" name="name" class="form-control" placeholder="Имя *" required="required" data-error="Поле обязательно для заполнения.">
             <div class="help-block with-errors"></div>
-            <input id="form_email" type="email" name="email" class="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required.">
+            <input id="form_email" type="email" name="email" class="form-control" placeholder="E-mail *" required="required" data-error="Введите адрес в корректной форме.">
             <div class="help-block with-errors"></div>
-            <input id="form_phone" type="tel" name="phone" class="form-control" placeholder="Please enter your phone">
+            <input id="form_phone" type="tel" name="phone" class="form-control" placeholder="Телефон">
             <div class="help-block with-errors"></div>
-            <textarea id="form_message" name="message" class="form-control" placeholder="Message for me *" rows="4" required="required" data-error="Please,leave us a message."></textarea>
+            <textarea id="form_message" name="message" class="form-control" placeholder="Текст сообщения *" rows="4" required="required" data-error="Пожалуйста, введите текст сообщения."></textarea>
             <div class="help-block with-errors"></div>
+            <input type="submit" class="btn btn-success btn-send" value="Send message">
         </div>    
     </div>    
+    </div>
+
+  </form>
   </div>
   <div class="col-sm-3">
     <ul class="nav flex-column">        
@@ -55,3 +64,8 @@
   </div>
 </div>
 <footer>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.9/validator.min.js" integrity="sha256-dHf/YjH1A4tewEsKUSmNnV05DDbfGN3g7NMq86xgGh8=" crossorigin="anonymous"></script>        
+<script src="contact.js"></script>
